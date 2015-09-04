8d15132c6bd5f7d2f97bdc316971c1f933d4aa84
Frank Calise <fcalise@gmail.com>
2015-09-04 02:00:52 -0400

    Removed unused files

--- Sample/XamDroid.ExpandableRecyclerView.Sample/Resources/Resource.designer.cs
+++ Sample/XamDroid.ExpandableRecyclerView.Sample/Resources/Resource.designer.cs
@@ -107,41 +107,32 @@ namespace XamDroid.ExpandableRecyclerView.Sample
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050005
-			public const int CrimeRecyclerView = 2131034117;
+			// aapt resource value: 0x7f050002
+			public const int CrimeRecyclerView = 2131034114;
 			
 			// aapt resource value: 0x7f050001
 			public const int FragmentContainer = 2131034113;
 			
-			// aapt resource value: 0x7f05000b
-			public const int action_settings = 2131034123;
-			
-			// aapt resource value: 0x7f050006
-			public const int child_list_item_crime_date_text_view = 2131034118;
-			
-			// aapt resource value: 0x7f050007
-			public const int child_list_item_crime_solved_check_box = 2131034119;
+			// aapt resource value: 0x7f050008
+			public const int action_settings = 2131034120;
 			
 			// aapt resource value: 0x7f050003
-			public const int crime_date = 2131034115;
+			public const int child_list_item_crime_date_text_view = 2131034115;
 			
 			// aapt resource value: 0x7f050004
-			public const int crime_solved = 2131034116;
-			
-			// aapt resource value: 0x7f050002
-			public const int crime_title = 2131034114;
+			public const int child_list_item_crime_solved_check_box = 2131034116;
 			
 			// aapt resource value: 0x7f050000
 			public const int item_touch_helper_previous_elevation = 2131034112;
 			
-			// aapt resource value: 0x7f05000a
-			public const int myButton = 2131034122;
+			// aapt resource value: 0x7f050007
+			public const int myButton = 2131034119;
 			
-			// aapt resource value: 0x7f050008
-			public const int parent_list_item_crime_title_text_view = 2131034120;
+			// aapt resource value: 0x7f050005
+			public const int parent_list_item_crime_title_text_view = 2131034117;
 			
-			// aapt resource value: 0x7f050009
-			public const int parent_list_item_expand_arrow = 2131034121;
+			// aapt resource value: 0x7f050006
+			public const int parent_list_item_expand_arrow = 2131034118;
 			
 			static Id()
 			{
@@ -160,19 +151,16 @@ namespace XamDroid.ExpandableRecyclerView.Sample
 			public const int ActivityFragment = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int FragmentCrime = 2130903041;
+			public const int FragmentCrimeList = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int FragmentCrimeList = 2130903042;
+			public const int ListItemCrimeChild = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int ListItemCrimeChild = 2130903043;
+			public const int ListItemCrimeParent = 2130903043;
 			
 			// aapt resource value: 0x7f030004
-			public const int ListItemCrimeParent = 2130903044;
-			
-			// aapt resource value: 0x7f030005
-			public const int Main = 2130903045;
+			public const int Main = 2130903044;
 			
 			static Layout()
 			{
